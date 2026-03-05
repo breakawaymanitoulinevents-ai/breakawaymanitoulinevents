c6ca96681e5b87e328e6c2bc24dcc31637c914f1
breakawaymanitoulinevents-ai <breakawaymanitoulin.events@gmail.com>
2026-03-05 08:51:52 -0500

    Delete site_v3 directory

--- site_v3/assets/script.js
+++
@@ -1,8 +0,0 @@
-// Small UX: highlight current nav link
-(function(){
-  const path = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('[data-nav]').forEach(a=>{
-    const href = a.getAttribute('href');
-    if(href === path || (path === '' && href === 'index.html')) a.style.background='rgba(0,0,0,.04)';
-  });
-})();
